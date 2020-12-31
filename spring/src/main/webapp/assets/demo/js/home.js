05f0accf2df6078652cf974f677507a9d4810f92
761472032@qq.com <D735322yun>
2020-12-31 11:35:07 +0800

    init

--- spring/src/main/webapp/assets/demo/js/home.js
+++ spring/src/main/webapp/assets/demo/js/home.js
@@ -2,6 +2,48 @@ var app = {
     method: {
         init: function () {
             app.$('title').innerText = 'This is home page.';
+            app.method.way1();
+        },
+        way1: function() {
+            fetch('/demo/data/list')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                }).catch(function (e) {
+                    console.log(e);
+                });
+        },
+        way2: function() {
+        },
+        way3: function() {
+            $.ajax({
+                url: '/demo/data/list',
+                type: 'GET',
+                dataType: 'text',
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        },
+        way4: function(url) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('GET', url, true);    //准备向服务器的ajaxtext1.ashx发送get请求
+            //监听onreadystatechange事件
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState === 4) {
+                    if (xmlhttp.status === 200) {
+                        console.log(xmlhttp.responseText);  //responseText属性为服务器返回的文本
+                    } else {
+                        console.log('AJAX服务器返回错误！');
+                    }
+                }
+            }
+            xmlhttp.send();     //开始发送请求，之前的代码都是做准备
         }
     },
     $: function (id) {
