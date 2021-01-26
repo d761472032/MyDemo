a20b531584e4caabead303ce56b28d04222dec74
761472032@qq.com <D735322yun>
2021-01-26 10:41:35 +0800

    init

--- javaDemo/src/main/java/com/demo/kafka/Consumer.java
+++ javaDemo/src/main/java/com/demo/kafka/Consumer.java
@@ -1,7 +1,41 @@
 package com.demo.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 /**
  * 消费者
  */
 public class Consumer {
+
+    public static void main(String[] args) {
+        //设置消费组的名称
+        //将属性值反序列化
+        Properties properties = new Properties();
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("bootstrap.servers", "192.168.1.13:9092,192.168.1.14:9092，192.168.1.15:9092");
+        properties.put("group.id", "test-consumer-group");
+
+        try (//创建一个消费者客户端实例
+             KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);) {
+            //订阅主题
+            consumer.subscribe(Collections.singletonList("demo"));
+            //循环消费消息
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(500));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("receiver a message from consumer client:" + record.value());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- javaDemo/src/main/java/com/demo/kafka/Producer.java
+++ javaDemo/src/main/java/com/demo/kafka/Producer.java
@@ -1,7 +1,47 @@
 package com.demo.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 /**
  * 生产者
  */
 public class Producer {
+
+    public static void main(String[] args) {
+        // 1.创建配置对象
+        Properties properties = new Properties();
+
+        // 2.设置配置信息
+        // kafka 服务端的主机名和端口号
+        properties.put("bootstrap.servers", "192.168.1.13:9092,192.168.1.14:9092，192.168.1.15:9092");
+        // 等待所有副本的应答
+        properties.put("acks", "all");
+        // 消息发送最大的尝试次数
+        properties.put("retries", 0);
+        // 一批消息的处理大小
+        properties.put("batch.size", 16384);
+        // 请求的延迟
+        properties.put("linger.ms", 1);
+        // 发送缓冲区内存大小
+        properties.put("buffer.size", 33554432);
+        // key 序列化
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // value 序列化
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        try (// KafkaProducer 有多个构造方法，可以用Map来进行社会参数，也可在构造方法中进行设置序列化
+             KafkaProducer<String, String> producer = new KafkaProducer<>(properties);) {
+            // 3生产数据
+            for (int i = 0; i < 30; i++) {
+                // ProducerRecord 中还可以设置 topic partition 时间戳 header 等信息
+                producer.send(new ProducerRecord<>("demo", Integer.toString(i), "helllo world-" + i));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
